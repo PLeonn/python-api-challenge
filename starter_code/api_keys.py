5d61c7e58e9ab14b2a692239b1608621b0c0a0da
Pleon <patrickedwardleon@gmail.com>
2020-08-05 22:01:31 -0700

    Python-Api-Challenge final

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bd08895fb2aa67e0b7a1a27657d1e397"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyClKCwO8pD5P1YkdiiVwakx3OoIzlcePeI"
